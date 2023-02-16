36e325f73eeedee9348b55ca7a363e1864759e97
unknown <kumarjitdas43@gmail.com>
2023-02-16 22:23:05 +0530

    update 16.2.23

--- myapp/src/components/ListTodos.js
+++ myapp/src/components/ListTodos.js
@@ -1,25 +1,46 @@
 import React, { useState } from 'react'
 import './todoliststyle.css'
 export default function ListTodos() {
-    const arr = ["Khana Khana aasdasds", "Padhai karna","Assignment baki hain", "Date pe jana hai", "Sona hai", "new Job"]
-  
+    // const arr = ["Khana Khana", "Padhai karna","Assignment baki hain", "Date pe jana hai", "Sona hai", "new Job"]
+    const [jobs, setJobs] = useState(["Khana Khana", "Padhai karna","Assignment baki hain", "Date pe jana hai", "Sona hai", "new Job"])
     const [display, setDisplay] = useState('hidden')
+    const [text, setText] = useState('')
+    let add=()=>{
+        // setJobs( (arr) => {
+        //     arr.push(text)
+        //     return arr
+        // })
+        setJobs((prev)=>{
+            return [...prev, text]
+        })
+        setText('')
+    }
+
     return (
         <div>
-            <h1>Todo App</h1>
-            <input type="text"/>
-            <button>Add</button>
+            <h1 style={{fontWeight:'300', textAlign:'center'}}>Todo App</h1>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+                <input type="text" className="textbox" value={text} onChange={(event)=>setText(event.target.value)}/>
+                <span style={{width:'16px'}}></span>
+                <button className='button2' onClick={add}>Add</button>
+            </div>
+            
             <hr />
             <div className='todo_container'>
                 {
-                    arr.map((element, index)=>{
-                        return <div key={index} className='single_job' onMouseEnter={()=>{setDisplay('show')}} onMouseLeave={()=>{setDisplay('hidden')}}>
-                                    <div>
-                                        <input type="checkbox" className={display}/>
+                    jobs.map((element, index)=>{
+                        // return <div key={index} className='single_job' onMouseEnter={()=>{setDisplay('show')}} onMouseLeave={()=>{setDisplay('hidden')}}>
+                        return  <div key={index} className='single_job'>
+                                    <div style={{display:'flex'}}>
+                                        <div style={{width:'30px'}} >
+                                            {/* <input type="checkbox" className={display}/> */}
+                                            <input type="checkbox" className='view'/>
+                                        </div>
                                         <span>{element}</span>
                                         
                                     </div>
-                                    <button className={display}>delete</button>
+                                    {/* <button className={display}>delete</button> */}
+                                    <button className="view button2">delete</button>
                                     
                                 </div>
                     })
