b0892406a6e4aefa9f8a769ee802cd32d54714b0
unknown <kumarjitdas43@gmail.com>
2023-02-17 20:02:12 +0530

    update 17.2.23

--- myapp/src/components/ListTodos.js
+++ myapp/src/components/ListTodos.js
@@ -2,20 +2,66 @@ import React, { useState } from 'react'
 import './todoliststyle.css'
 export default function ListTodos() {
     // const arr = ["Khana Khana", "Padhai karna","Assignment baki hain", "Date pe jana hai", "Sona hai", "new Job"]
-    const [jobs, setJobs] = useState(["Khana Khana", "Padhai karna","Assignment baki hain", "Date pe jana hai", "Sona hai", "new Job"])
+    // const [jobs, setJobs] = useState(["Khana Khana", "Padhai karna","Assignment baki hain", "Date pe jana hai", "Sona hai", "new Job"])
+    const [jobs, setJobs] = useState([
+        {
+            desc:'Khana Khana',
+            completed:false,
+        },
+        {
+            desc:'Padhai karna',
+            completed:true,
+        },
+        {
+            desc:'Assignment baki hain',
+            completed:false,
+        }
+    ])
     const [display, setDisplay] = useState('hidden')
     const [text, setText] = useState('')
-    let add=()=>{
+    const add=()=>{
         // setJobs( (arr) => {
         //     arr.push(text)
         //     return arr
         // })
+        const data = {
+            desc:text,
+            completed:false
+        }
         setJobs((prev)=>{
-            return [...prev, text]
+            return [...prev, data]
         })
         setText('')
     }
 
+    const remove=(index)=>{
+        // setJobs( (prev)=>{
+        //     return [...prev.slice(0, index), ...prev.slice(index+1)]
+        // } )
+        setJobs( prev => prev.filter((ele, ind)=>ind!=index) )
+    }
+
+    const handleComplete=(index)=>{
+        //console.log(index)
+        //setJobs( (prev) => {
+            // prev[index] = {...prev[index], completed:true}
+            // return prev
+            // return prev.map((element, ind)=>{
+            //     if(ind == index)
+            //         return {...element, completed:!element.completed }
+            //     else 
+            //         return element
+            // })
+        //} )
+        setJobs( (arr) => {
+            return arr.map( (element, ind)=> {
+                if(ind == index)
+                    return {...element, completed:!element.completed}
+                else
+                    return element
+            } )
+        } )
+    }
     return (
         <div>
             <h1 style={{fontWeight:'300', textAlign:'center'}}>Todo App</h1>
@@ -23,6 +69,14 @@ export default function ListTodos() {
                 <input type="text" className="textbox" value={text} onChange={(event)=>setText(event.target.value)}/>
                 <span style={{width:'16px'}}></span>
                 <button className='button2' onClick={add}>Add</button>
+                {/* <button className='button2' onClick={()=>{
+                    setJobs((prev)=>{
+                        return [...prev, text]
+                    })
+                    setText('')
+                }}>
+                    Add
+                </button> */}
             </div>
             
             <hr />
@@ -34,13 +88,13 @@ export default function ListTodos() {
                                     <div style={{display:'flex'}}>
                                         <div style={{width:'30px'}} >
                                             {/* <input type="checkbox" className={display}/> */}
-                                            <input type="checkbox" className='view'/>
+                                            <input type="checkbox" className='' checked={element.completed} onChange={()=>handleComplete(index)}/>
                                         </div>
-                                        <span>{element}</span>
+                                        <span style={{textDecoration: element.completed==true?'line-through':''}}>{element.desc}</span>
                                         
                                     </div>
                                     {/* <button className={display}>delete</button> */}
-                                    <button className="view button2">delete</button>
+                                    <button className="view button2" onClick={()=>remove(index)}>delete</button>
                                     
                                 </div>
                     })
